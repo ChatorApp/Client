200d72b71759d6f1af43ab930b24e7825f5e7b4e
Ben Hall <benmhall1998@gmail.com>
2020-07-19 22:11:16 +0100

    :construction: Started work on the action component

---
+++ src/components/generic/Action.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="action">
+    <p class="text-center">{{ Title }}</p>
+    <div :style="actionIconStyle"></div>
+    <v-btn :to="ButtonUrl">{{ ButtonText }}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Action',
+  props: {
+    Title: String,
+    Icon: String,
+    ButtonText: String,
+    ButtonUrl: String,
+  },
+  computed: {
+    actionIconStyle() {
+      return {
+        width: '100%',
+        margin: '26px 0',
+        'background-position': '50%',
+        'background-repeat': 'no-repeat',
+        'background-image': `url(/assets/${this.Icon}`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.action {
+    width: 228px;
+    max-width: 228px;
+    text-align: center;
+    padding: 16px;
+    overflow: hidden;
+    cursor: pointer;
+    border: 1px solid;
+}
+</style>
