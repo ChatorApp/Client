3b9c86a027d274ed7391401e59a4e0049a3ca3cb
Ben Hall <benmhall1998@gmail.com>
2020-07-21 22:36:21 +0100

    :sparkles: Introduced the new server creation menu

--- src/components/servers/CreateJoinModal.vue
+++ src/components/servers/CreateJoinModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="290">
+  <v-dialog v-model="dialog" persistent max-width="30%">
     <template v-slot:activator="{ on, attrs }">
       <v-list-item v-bind="attrs" v-on="on">
         <v-list-item-avatar>
@@ -14,13 +14,11 @@
         <v-container>
           <h1 class="text-center">Create or Join a Server</h1>
           <v-row>
-            <v-col cols="12" sm="6">
-              <Action
-                title="<strong>Create</strong> a server!"
-                Icon="account-multiple-plus"
-                ButtonText="Create a server"
-                ButtonUrl="/test"
-              ></Action>
+            <v-col cols="12" sm="6" align="center">
+              <CreateServerDialog></CreateServerDialog>
+            </v-col>
+            <v-col cols="12" sm="6" align="center">
+              <JoinServerDialog></JoinServerDialog>
             </v-col>
           </v-row>
         </v-container>
@@ -30,12 +28,19 @@
 </template>
 
 <script>
-import Action from '@/components/generic/Action.vue';
+import CreateServerDialog from './dialogs/CreateServerDialog.vue';
+import JoinServerDialog from './dialogs/JoinServerDialog.vue';
 
 export default {
   name: 'CreateJoinModal',
   components: {
-    Action,
+    CreateServerDialog,
+    JoinServerDialog,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
   },
 };
 </script>
