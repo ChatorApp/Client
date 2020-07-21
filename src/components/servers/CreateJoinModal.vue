15254e52e4c74ff5214c7ec9ce79e654a3b102ee
Ben Hall <benmhall1998@gmail.com>
2020-07-21 15:36:38 +0100

    :construction: Testing Generic components

--- src/components/servers/CreateJoinModal.vue
+++ src/components/servers/CreateJoinModal.vue
@@ -4,31 +4,38 @@
       <v-list-item v-bind="attrs" v-on="on">
         <v-list-item-avatar>
           <v-avatar color="indigo" style="min-width: 0px;">
-              <v-icon>mdi-server-network</v-icon>
+            <v-icon>mdi-server-network</v-icon>
           </v-avatar>
         </v-list-item-avatar>
       </v-list-item>
     </template>
     <v-card>
-      <v-card-title>Create or Join a Server</v-card-title>
       <v-card-text>
-          <v-container>
-              <v-row>
-                  <v-col cols="12" sm="6">
-                      Create a server
-                  </v-col>
-                  <v-col cols="12" sm="6">
-                      Join a server
-                  </v-col>
-              </v-row>
-          </v-container>
+        <v-container>
+          <h1 class="text-center">Create or Join a Server</h1>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <Action
+                title="<strong>Create</strong> a server!"
+                Icon="account-multiple-plus"
+                ButtonText="Create a server"
+                ButtonUrl="/test"
+              ></Action>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import Action from '@/components/generic/Action.vue';
+
 export default {
   name: 'CreateJoinModal',
+  components: {
+    Action,
+  },
 };
 </script>
