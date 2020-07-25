f86b5f0dfd22122f7749ee6d575d8e6ce0e6ba8f
Ben Hall <benmhall1998@gmail.com>
2020-07-25 17:00:50 +0100

    :construction: Started to implement the server channels bar

---
+++ src/components/servers/ServerChannelsBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <ChannelPanel>
+    <v-list-item>
+      <v-col cols="12" sm="2" justify="center" align="center">
+        <v-icon>mdi-check-decagram</v-icon>
+      </v-col>
+      <v-col cols="12" sm="10" justify="center" align="left">{{ Name }}</v-col>
+    </v-list-item>
+    <v-divider></v-divider>
+  </ChannelPanel>
+</template>
+
+<script>
+import ChannelPanel from '../layout/ChannelPanel.vue';
+
+export default {
+  name: 'ServerChannelsBar',
+  components: {
+    ChannelPanel,
+  },
+  props: ['Name'],
+};
+</script>
+
+<style>
+</style>
